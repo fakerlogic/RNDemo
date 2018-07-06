16ebdfa21b4bf230674eced9eff6361a73d4d345
kimi <caoxz@ap-ec.cn>
2018-07-06 16:16:04 +0800

    初始化工程

--- RNHeyGuys/src/Components/ShopCartItem.js
+++ RNHeyGuys/src/Components/ShopCartItem.js
@@ -2,13 +2,68 @@
 import React from 'react'
 import {View, Text, StyleSheet} from 'react-native'
 
-class ShopCartItem extends React.Component {
 
-  props: {
-    
-  }
+const ShopCartItemType {
+
+}
+
+export default class ShopCartItem extends React.Component {
+
+  
 
   render() {
-    
+    const {
+      id,
+      type,
+      onPress,
+      selected,
+      skuImage,
+      spuName,
+      skuName,
+      price,
+      minNum,
+      maxNum,
+      number
+    } = this.props
+
+    return (
+
+    )
   }
+
+  renderSelectButton = (selected) => {
+    return (
+
+    )
+  }
+
+  renderSkuImage = (skuImageUrl) => {
+    return (
+
+    )
+  }
+
+  rendertitle = (title) => {
+    return (
+
+    )
+  }
+
+  renderSubtitle = (subTitle) => {
+    return (
+
+    )
+  }
+
+  renderPrice = (price) => {
+    return (
+
+    )
+  }
+
+
 }
+
+const styles = StyleSheet.create({
+  
+})
